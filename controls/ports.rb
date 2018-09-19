01d73cb994f856a8c1966f73e6602da7af2c56c6
Ger Van Hees <Ger@Gers-MacBook-Pro.local>
2018-09-19 14:34:59 +1200

    aaaaa

--- controls/ports.rb
+++ controls/ports.rb
@@ -1,3 +1,15 @@
 describe port(80) do
   it { should_not be_listening }
 end
+describe port(8089) do
+  it { should be_listening }
+end
+describe port(8000) do
+  it { should be_listening }
+end
+describe port(22) do
+  it { should be_listening }
+end
+describe port(23) do
+  it { should_not be_listening }
+end
--- controls/process.rb
+++ controls/process.rb
@@ -1,3 +1,6 @@
 describe processes(Regexp.new("splunkd -p 8089.*start$")) do
   its('entries.length') { should eq 1 }
 end
+describe processes(Regexp.new("mongod --dbpath=/opt/splunk/var/lib/splunk/kvstore/mongo")) do
+  its('entries.length') { should eq 1 }
+end
